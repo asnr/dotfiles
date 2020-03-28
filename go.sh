72750d420d6fba9675abb51116133a69aaff06b6
asnr <asnr@users.noreply.github.com>
2020-03-28 06:15:58 -0700

    vscode: store vscode settings in dotfiles (only for macos)
    The go.sh script required modification to work with filenames with spaces in
    them.
    
    This only works for macos as VSCode looks for the settings.json file in a
    different file path :C

--- go.sh
+++ go.sh
@@ -9,22 +9,23 @@ echo
 
 LINK_DIR=link
 
-for file_to_link in $(find $LINK_DIR ! -name '*~' ! -name '*.swp' -type f); do
-  SOURCE_PATH=$DOTFILES_DIR/$file_to_link
-  LINK_SUFFIX=${file_to_link#$LINK_DIR/}
-  LINK_TO_CREATE=$HOME/$LINK_SUFFIX
-  PRETTY_LINK_TO_CREATE=~/$LINK_SUFFIX
+find $LINK_DIR ! -name '*~' ! -name '*.swp' -type f | while read file_to_link
+do
+  SOURCE_PATH="$DOTFILES_DIR/$file_to_link"
+  LINK_SUFFIX="${file_to_link#$LINK_DIR/}"
+  LINK_TO_CREATE="$HOME/$LINK_SUFFIX"
+  PRETTY_LINK_TO_CREATE="~/$LINK_SUFFIX"
 
   # If symlink already exists, do nothing. If file exists, back it up
   # else create link
-  if test $SOURCE_PATH -ef $LINK_TO_CREATE; then
+  if test "$SOURCE_PATH" -ef "$LINK_TO_CREATE"; then
     echo "Link to $file_to_link already exists, moving on"
-  elif test ! -e $LINK_TO_CREATE; then
+  elif test ! -e "$LINK_TO_CREATE"; then
     echo "Creating link $PRETTY_LINK_TO_CREATE -> $file_to_link"
-    if test ! -d $(dirname $LINK_TO_CREATE); then
-      mkdir -p $(dirname $LINK_TO_CREATE)
+    if test ! -d "$(dirname "$LINK_TO_CREATE")"; then
+      mkdir -p "$(dirname "$LINK_TO_CREATE")"
     fi
-    ln -s $SOURCE_PATH $LINK_TO_CREATE
+    ln -s "$SOURCE_PATH" "$LINK_TO_CREATE"
   else
     echo "$PRETTY_LINK_TO_CREATE already exists but is not a symlink to $SOURCE_PATH"
     echo "Aborting"
