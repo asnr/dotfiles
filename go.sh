ae383a145a3a36ffd340829288475b79ef73a84c
asnr <asnr@users.noreply.github.com>
2025-02-27 08:30:07 +1100

    go.sh: refactor, extract create_soft_link function
    In preparation for reusing it

--- go.sh
+++ go.sh
@@ -9,17 +9,11 @@ echo
 
 LINK_DIR=link
 
-find $LINK_DIR ! -name '*~' ! -name '*.swp' -type f | while read file_to_link
-do
-  SOURCE_PATH="$DOTFILES_DIR/$file_to_link"
-  LINK_SUFFIX="${file_to_link#$LINK_DIR/}"
+function create_soft_link() {
+  local SOURCE_SUFFIX="$1"
+  local LINK_SUFFIX="$2"
 
-  # Need this workaround because VSCode expects settings.json to be in different
-  # directories depending on the OS.
-  if [ "$LINK_SUFFIX" = '.config/Code/User/settings.json' -a \
-       "$(uname)" = "Darwin" ]; then
-    LINK_SUFFIX='Library/Application Support/Code/User/settings.json'
-  fi
+  SOURCE_PATH="$DOTFILES_DIR/$SOURCE_SUFFIX"
 
   LINK_TO_CREATE="$HOME/$LINK_SUFFIX"
   PRETTY_LINK_TO_CREATE="~/$LINK_SUFFIX"
@@ -29,7 +23,7 @@ do
   if test "$SOURCE_PATH" -ef "$LINK_TO_CREATE"; then
     echo $'\342\234\224' "$PRETTY_LINK_TO_CREATE" # $'\342\234\224' = ✔
   elif test ! -e "$LINK_TO_CREATE"; then
-    echo "Creating link $PRETTY_LINK_TO_CREATE -> $file_to_link"
+    echo "Creating link $PRETTY_LINK_TO_CREATE -> $SOURCE_SUFFIX"
     if test ! -d "$(dirname "$LINK_TO_CREATE")"; then
       mkdir -p "$(dirname "$LINK_TO_CREATE")"
     fi
@@ -39,6 +33,20 @@ do
     echo "Aborting"
     exit 1
   fi
+}
+
+find $LINK_DIR ! -name '*~' ! -name '*.swp' ! -name '*DS_Store' -type f | while read file_to_link
+do
+  LINK_SUFFIX="${file_to_link#$LINK_DIR/}"
+
+  # Need this workaround because VSCode expects settings.json to be in different
+  # directories depending on the OS.
+  if [ "$LINK_SUFFIX" = '.config/Code/User/settings.json' -a \
+       "$(uname)" = "Darwin" ]; then
+    LINK_SUFFIX='Library/Application Support/Code/User/settings.json'
+  fi
+
+  create_soft_link "$file_to_link" "$LINK_SUFFIX"
 done
 
 echo
